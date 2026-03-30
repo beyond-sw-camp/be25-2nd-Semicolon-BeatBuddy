f92d0b02e99013830f15425fca2278c1b9788947
bbangjee <qkdwl9584@naver.com>
2026-03-31 00:48:06 +0900

    feat: exception handler 모든 케이스 처리

--- src/main/java/com/beyond/beatbuddy/global/config/SecurityConfig.java
+++ src/main/java/com/beyond/beatbuddy/global/config/SecurityConfig.java
@@ -53,6 +53,8 @@ public class SecurityConfig {
 								"/api/v1/auth/login",
 								"/api/v1/auth/email/**",
 								"/api/v1/auth/token/refresh",
+								"/api/v1/auth/password/email/send",
+								"/api/v1/auth/password/reset",
 								"/default-profile.png",
 								"/default-group.png",
 								"/swagger-ui/**",
--- src/main/java/com/beyond/beatbuddy/global/exception/ConflictException.java
+++ src/main/java/com/beyond/beatbuddy/global/exception/ConflictException.java
@@ -1,7 +1,7 @@
 package com.beyond.beatbuddy.global.exception;
 
 import org.springframework.http.HttpStatus;
-
+// 409 - 중복 (이메일, 닉네임)
 public class ConflictException extends BusinessException {
 	public ConflictException(String message) {
 		super(HttpStatus.CONFLICT, message);
